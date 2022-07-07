e99bc4fdebe290cd2953f427b4a204e80d06f97c
Josh <91155952+thecoffeeroaster@users.noreply.github.com>
2022-07-07 15:59:41 -0700

    Create a main object. Create an AppWindow object and plug in the main object so that they can communicate with each other. The main object will handle the instances of windows (if in the future we want multiple instance support).

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Main main = new Main();
+        JFrame appWindow = new AppWindow(main);
+    }
 
+    public void generateNewBarGraph(AppWindow appWindow, int numberOfBars)
+    {
+        ArrayList<Bar> barArrayList = new ArrayList<>();
+        //appWindow.setBarPanelGridLayout();
     }
 }
